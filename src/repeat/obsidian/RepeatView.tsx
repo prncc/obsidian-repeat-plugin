76fd66c5e72f549015060180def8e8feb134a4bc
Andre Perunicic <perunicic@gmail.com>
2022-09-18 22:33:16 -0400

    Implement a sketch of the repetition interface

--- src/repeat/obsidian/RepeatView.tsx
+++ src/repeat/obsidian/RepeatView.tsx
@@ -1,6 +1,10 @@
 import { DateTime } from 'luxon';
-import { ItemView, WorkspaceLeaf } from 'obsidian';
-import { getAPI, Literal } from 'obsidian-dataview';
+import { Component, ItemView, WorkspaceLeaf, MarkdownPreviewView, TFile } from 'obsidian';
+import { getAPI, Literal, DataviewApi } from 'obsidian-dataview';
+import { determineFrontmatterBounds, replaceOrInsertField } from 'src/frontmatter';
+import { getRepeatChoices } from '../choices';
+import { parseRepetitionFields } from '../parsing';
+import { RepeatChoice } from '../repeatTypes';
 
 export const REPEATING_NOTES_DUE_VIEW = 'repeating-notes-due-view';
 
@@ -11,12 +15,41 @@ function isNoteDue(repeatDueAt: Literal | string | undefined): boolean {
   return repeatDueAt <= DateTime.now();
 }
 
+function getNextDueNote(
+  dv: DataviewApi | undefined,
+): Record<string, Literal> | undefined {
+  const page = dv?.pages()
+    .where(({ repeat_due_at }) => isNoteDue(repeat_due_at))
+    .sort(({ repeat_due_at }) => repeat_due_at, 'asc')
+    .first();
+  if (!page) { return; }
+  return page;
+}
+
 class RepeatView extends ItemView {
   root: Element;
+  component: Component;
+  messageContainer: HTMLElement;
+  buttonsContainer: HTMLElement;
+  previewContainer: HTMLElement;
+  loaded: boolean;
+  dv: DataviewApi | undefined;
 
   constructor(leaf: WorkspaceLeaf) {
     super(leaf);
+    this.component = new Component();
+
+    this.dv = getAPI(this.app);
+
     this.root = this.containerEl.children[1];
+    this.messageContainer = this.root.createDiv();
+    this.buttonsContainer = this.root.createDiv();
+    this.previewContainer = this.root.createEl('div', { cls: 'repeat-embedded_note' });
+    this.loaded = false;
+
+    this.addRepeatButton = this.addRepeatButton.bind(this);
+    this.setPage = this.setPage.bind(this);
+    this.resetContainers = this.resetContainers.bind(this);
   }
 
   getViewType() {
@@ -28,38 +61,111 @@ class RepeatView extends ItemView {
   }
 
   async onOpen() {
-    const dv = getAPI(this.app);
-    if (!dv) {
-      let container = this.root.createEl('div')
-      container.setText(
+    if (!this.dv) {
+      this.messageContainer.setText(
         'Repeat Plugin requires DataView Plugin to work. ' +
         'Make sure it is installed and enabled.'
       )
       return;
     }
-    let processed = false;
-    const setPage = async () => {
-      const pages = await dv?.pages()
-          .where(({ repeat_due_at }) => isNoteDue(repeat_due_at))
-          .sort(({ repeat_due_at }) => repeat_due_at, 'asc')
-          .file.name;
-        let container = this.root.createEl('div');
-        container.setText(
-          `file: ${pages ? pages[0] : ''}`);
-        console.log(pages ? pages[0] : '');
-        processed = true;
-    }
-
     this.registerEvent(
       // @ts-ignore: event is added by DataView.
       app?.metadataCache.on('dataview:index-ready', async () => {
-        setPage();
+        if (!this.loaded) {
+          this.setPage();
+        }
       })
     );
-
-    if (dv?.index.initialized && !processed) {
-      setPage();
+    if (this.dv?.index.initialized && !this.loaded) {
+      this.setPage();
     }
+  }
+
+  async setPage() {
+    this.messageContainer.setText('hello');
+    const page = getNextDueNote(this.dv);
+    if (!page) {
+      this.messageContainer.setText('All done for now!');
+      this.loaded = true;
+      return;
+    }
+    const dueFilePath = (page?.file as any).path;
+    const repetition = parseRepetitionFields(
+      (page.repeat || '') as string, page.repeat_due_at as string);
+    const choices = getRepeatChoices(repetition);
+
+    const matchingMarkdowns = this.app.vault.getMarkdownFiles()
+      .filter((file) => file?.path === dueFilePath);
+    if (!matchingMarkdowns) {
+      this.messageContainer.setText(
+        `Error: Could not find due note ${dueFilePath}. ` +
+        'Reopen this view to retry and please report any bugs.');
+      this.loaded = true;
+      return;
+    }
+    const file = matchingMarkdowns[0];
+    choices.forEach(choice => this.addRepeatButton(choice, file));
+    const markdown = await this.app.vault.cachedRead(file);
+    await MarkdownPreviewView.renderMarkdown(
+      markdown,
+      this.previewContainer,
+      file.path,
+      this.component,
+    );
+    this.loaded = true;
+  }
+
+  resetContainers() {
+    this.buttonsContainer.setText('');
+    this.buttonsContainer.innerHTML = '';
+    this.previewContainer.innerHTML = '';
+  }
+
+  async addRepeatButton(
+    choice: RepeatChoice,
+    file: TFile,
+  ) {
+    let button = this.buttonsContainer.createEl('button', {
+        text: choice.text,
+      },
+      (buttonElement) => {
+        buttonElement.onclick = async (event) => {
+          this.resetContainers();
+          const originalMarkdown = await this.app.vault.read(file);
+          const bounds = determineFrontmatterBounds(originalMarkdown);
+          if (!bounds) {
+            throw Error('Could not find frontmatter in note.')
+          }
+          let frontmatter = originalMarkdown.slice(...bounds);
+          frontmatter = replaceOrInsertField(
+            frontmatter, 'repeat_due_at',
+            DateTime.now().plus({ year: 1 }).toISO()
+          );
+          const newContent = [
+            originalMarkdown.slice(0, bounds[0]),
+            frontmatter,
+            originalMarkdown.slice(bounds[1])
+          ].join('');
+          new Promise((resolve) => {
+            this.app.vault.modify(file, newContent);
+            // @ts-ignore: event is added by DataView.
+            this.app.metadataCache.on('dataview:metadata-change',
+              (_, eventFile, __) => {
+                if (eventFile?.path === file.path) {
+                  resolve(null);
+                }
+              });
+            // Resolve no matter what to avoid getting stuck.
+            setTimeout(resolve, 100);
+          }).then(() => {
+            this.setPage();
+          });
+          // TODO: Make sure the current page doesn't get reloaded - the dv index has to update
+          // TODO: refactor this (sketch) into a method shared with the commands in main...
+
+        }
+      });
+    return button;
   }
 }
 
