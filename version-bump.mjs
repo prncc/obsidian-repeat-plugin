eb798c9a0bcb23b840a677ec8e24d80c603492ec
Andre Perunicic <perunicic@gmail.com>
2022-12-31 15:17:21 -0500

    Update version in package.json and use spaces instead of tabs

--- version-bump.mjs
+++ version-bump.mjs
@@ -6,9 +6,14 @@ const targetVersion = process.env.npm_package_version;
 let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
 const { minAppVersion } = manifest;
 manifest.version = targetVersion;
-writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
+writeFileSync("manifest.json", JSON.stringify(manifest, null, "  ") + "\n");
 
 // update versions.json with target version and minAppVersion from manifest.json
 let versions = JSON.parse(readFileSync("versions.json", "utf8"));
 versions[targetVersion] = minAppVersion;
-writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
+writeFileSync("versions.json", JSON.stringify(versions, null, "  ") + "\n");
+
+// update package.json
+let packageJson = JSON.parse(readFileSync("package.json", "utf8"));
+packageJson.version = targetVersion;
+writeFileSync("package.json", JSON.stringify(packageJson, null, "  ") + "\n");
